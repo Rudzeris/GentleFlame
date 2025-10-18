a8f5c9d8d8269660eceb6227ca24458c3b6fccdd
Rudzeris <ruzil1atangulov@gmail.com>
2025-10-18 17:36:20 +0500

    немного изменил SaveService и сцену

--- Assets/Scripts/Services/SaveService.cs
+++ Assets/Scripts/Services/SaveService.cs
@@ -2,8 +2,25 @@
 {
     public interface ISaveService
     {
+        void SaveGame();
+        void LoadGame();
+        void DeleteSave();
     }
     public class SaveService : ISaveService
     {
+        public void DeleteSave()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LoadGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveGame()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
